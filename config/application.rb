32c812633ed71a2affeaa3ff10924230afdd13ae
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-01-25 12:10:35 +0100

    S3_ACCESS_KEY S3_SECRET_KEY S3_BUCKET

--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,6 @@ module PhotoApp
     config.active_record.raise_in_transactional_callbacks = true
 
 
-    AWS_ACCESS_KEY = "AKIAJB3NOXHCAE4PF27A"
-
-    AWS_SECRET_KEY = "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"
-
-    AWS_BUCKET = "phappimage"
 
   end
 end
