b4faaacb6c90c241586ece0effc938338f5f7bc8
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-01-25 12:16:59 +0100

    S3_ACCESS_KEY S3_SECRET_KEY S3_BUCKET

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -20,7 +20,8 @@ if Rails.env.production?
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"] ,
+    :region                 => ENV["AWS_REGION"]       # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]  
 
